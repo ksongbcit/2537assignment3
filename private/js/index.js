a54acd872c392c8458a2a4d7cd82246ce633139a
Kyung Min Song <ksong34@my.bcit.ca>
2021-05-11 11:37:30 -0700

    added some styling to index.css

--- private/js/index.js
+++ private/js/index.js
@@ -1,24 +1,27 @@
 "use strict";
-$(document).ready(function() {
-    $("#submit").click(function() {
+$(document).ready(function () {
+    $("#submit").click(function () {
         $.ajax({
             url: "/authenticate",
             type: "POST",
             dataType: "JSON",
-            data: { email: $("#email").val(), password: $("#password").val() },
-            success: function(data) {
-                if(data['status'] == "success") {
+            data: {
+                email: $("#email").val(),
+                password: $("#password").val()
+            },
+            success: function (data) {
+                if (data['status'] == "success") {
                     window.location.replace("/profile");
                 } else {
                     $("#errorMsg").html(data['msg']);
                 }
 
             },
-            error: function(jqXHR, textStatus, errorThrown) {
-                $("body").text(jqXHR.statusText);
+            error: function (jqXHR, textStatus, errorThrown) {
+                $("#errorMsg").text(jqXHR.statusText);
             }
         });
 
     });
-    
+
 });
